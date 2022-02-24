ac0a4bd4da1a00ac74c583fa4d4eeca00c083372
Dewan Komputer <dewan.komputer@gmail.com>
2022-02-24 10:11:07 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <link rel="icon" href="dk.png">
+	<title>Dewan Bootstrap - Progress Bar</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+	<nav class="navbar navbar-dark bg-danger">
+	  <a class="navbar-brand" href="index.php" style="color: #fff;">
+	    Dewan Komputer
+	  </a>
+	</nav>
+
+	<div class="container mb-5">
+		<h3 align="center">Progress Bar Bootstrap 4</h3><hr>
+		<div class="progress mb-2">
+		  <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<hr>
+
+		<h4 align="center">Progress Bar </h4>
+		<div class="progress mb-2">
+		  <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%</div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
+		</div>
+		<hr>
+
+		<h4 align="center">Progress Bar Height </h4>
+		<div class="progress mb-2" style="height: 2px;">
+		  <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2" style="height: 20px;">
+		  <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<hr>
+
+		<h4 align="center">Progress Bar Style</h4>
+		<div class="progress mb-2">
+		  <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar bg-danger" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar bg-primary" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar bg-secondary" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar bg-dark" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<hr>
+
+		<h4 align="center">Multiple bars </h4>
+		<div class="progress mb-2">
+		  <div class="progress-bar" role="progressbar" style="width: 15%" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
+		  <div class="progress-bar bg-success" role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+		  <div class="progress-bar bg-info" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<hr>
+
+		<h4 align="center">Progress Bar Striped</h4>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+		</div>
+		<hr>
+
+		<h4 align="center">Progress Bar Animated Stripes</h4>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 25%"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped bg-danger progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 50%"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped bg-success progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped bg-info progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped bg-secondary progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+		</div>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped bg-dark progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+		</div>
+		<hr>
+
+		<h4 align="center">Menjalankan Progress bar</h4>
+		<div class="progress mb-2">
+		  <div class="progress-bar progress-bar-striped bg-primary progress-bar-animated" id="bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 1%"></div>
+		</div>
+		<button class="btn btn-success" onclick="jalan()">Jalankan</button> 
+	</div>
+
+	<div class="navbar bg-dark">
+		<div style="color: #fff;">© <?php echo date('Y'); ?> Copyright:
+		    <a href="https://dewankomputer.com/"> Dewan Komputer</a>
+		</div>
+	</div>
+
+	<script>
+		function jalan() {
+		  var elem = document.getElementById("bar");   
+		  var width = 1;
+		  var id = setInterval(frame, 10);
+		  function frame() {
+		    if (width >= 100) {
+		      clearInterval(id);
+		    } else {
+		      width++; 
+		      elem.style.width = width + '%'; 
+		    }
+		  }
+		}
+	</script>
+</body>
+</html>
